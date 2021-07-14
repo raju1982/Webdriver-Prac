c6cefc28966e4212de1a99902270c6270cdb951c
raju1982 <rakeshkandpal1982@gmail.com>
2021-07-13 21:51:25 -0700

    updated notes

--- src/test/java/keys/KeysTests.java
+++ src/test/java/keys/KeysTests.java
@@ -3,13 +3,15 @@ package keys;
 import base.BaseTests;
 import org.openqa.selenium.Keys;
 import org.testng.annotations.Test;
+import pages.KeyPressesPage;
+
 import static org.testng.Assert.assertEquals;
 
 public class KeysTests extends BaseTests {
 
     @Test
     public void testBackspace(){
-        var keyPage = homePage.clickKeyPresses();
+        KeyPressesPage keyPage = homePage.clickKeyPresses();
         keyPage.enterText("A" + Keys.BACK_SPACE);
         assertEquals(keyPage.getResult(), "You entered: BACK_SPACE");
     }
